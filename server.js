b21794eb8af4f7afb9aee4d028bdfe7a248ba325
Anubhav Sinha <anubhav.a.sinha@pwc.com>
2020-08-26 12:53:57 +0530

    acsd

--- server.js
+++ server.js
@@ -20,11 +20,17 @@ app.get('',(req,res)=>{
 app.get('/getWidgetDropdown',(req,res)=>{
     axios.get('http://104.40.75.137:9003/assets/data/data.json').then((response)=>{
         console.log(response.data.length);
+        console.log(response.data[0].widgetsContent[0].WidgetCode[0].CodeSnippet);
         var widgetNames = [];
+        var definitions = [];
         for (let i =0; i < response.data.length; i++){
             for (let j = 0; j < response.data[i].widgetsContent.length; j++){
                 widgetNames.push({'id': response.data[i].widgetsContent[j].id,
-                'name':response.data[i].widgetsContent[j].widgetsName});
+                'name':response.data[i].widgetsContent[j].widgetsName,
+            'imports':response.data[i].widgetsContent[j].WidgetCode[0].CodeSnippet,
+            'call':response.data[i].widgetsContent[j].WidgetCode[2].CodeSnippet,
+            'definitions':response.data[i].widgetsContent[j].WidgetCode[1].CodeSnippet.split(/\r?\n/) 
+        });
             }
         }
         res.send(widgetNames);

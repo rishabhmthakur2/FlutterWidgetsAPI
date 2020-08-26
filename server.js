c712fa3e1d2d7bbdf369a39da1d9055c8ab7f236
rishabhmthakur2 <rishabhmthakur@outlook.com>
2020-08-26 14:47:43 +0530

    Dynamic Parameters Backend

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 const axios = require('axios').default;
+const widgets = require('./parameters.json');
+const getWidgets = require('./test');
 
 var app = express();
 app.use(cors());
@@ -19,23 +21,39 @@ app.get('',(req,res)=>{
 
 app.get('/getWidgetDropdown',(req,res)=>{
     axios.get('http://104.40.75.137:9003/assets/data/data.json').then((response)=>{
-        console.log(response.data.length);
-        console.log(response.data[0].widgetsContent[0].WidgetCode[0].CodeSnippet);
         var widgetNames = [];
         var definitions = [];
         for (let i =0; i < response.data.length; i++){
             for (let j = 0; j < response.data[i].widgetsContent.length; j++){
                 widgetNames.push({'id': response.data[i].widgetsContent[j].id,
-                'name':response.data[i].widgetsContent[j].widgetsName,
-            'imports':response.data[i].widgetsContent[j].WidgetCode[0].CodeSnippet,
-            'call':response.data[i].widgetsContent[j].WidgetCode[2].CodeSnippet,
-            'definitions':response.data[i].widgetsContent[j].WidgetCode[1].CodeSnippet.split(/\r?\n/) 
+                'name':response.data[i].widgetsContent[j].widgetsName
         });
             }
         }
         res.send(widgetNames);
     });
 });
+
+app.get('/getWidgetParamaters?:widgetName', async (req, res)=>{
+    let widgetName = req.query.widgetName;
+    let widgetParams;
+    let widgetArray = widgets.widgets;
+    try{
+        const result = await widgetArray.find(widget => widget.name == widgetName);
+        res.status(200).json(result.paramaters);
+    }
+    catch (error){
+        res.status(400).send(error);
+    }
+})
+
+app.post('/submitWidgetParams', async(req, res)=>{
+    let widgetName = req.body.widgetName;
+    let paramsValue = req.body.params;
+    getWidgets(widgetName, paramsValue).then((data)=>{
+        res.send(data);
+    });
+})
 
 app.listen(port, ()=>{
     console.log('Listening on port:' + port);
---
+++ test.js
@@ -0,0 +1,35 @@
+let getWidgets = (widgetName, paramValues) => new Promise((resolve, reject)=>{
+    let widgetList = [
+        {
+          id: 10,
+          name: 'FloatingActionButton',
+          imports:
+            "import 'package:rapid_widgets_library/widget-classesUsed.dart'; \nimport 'package:rapid_widgets_library/custom-floatingActionButton.dart';",
+          call: 'customFloatingActionButton(btnFloatObject, onFloatButtonPress),',
+          definitions: `
+                  onFloatButtonPress(bool b){ \n,
+                  print(b); \n,
+                  } \n,
+                  Floatingactionbuttonparam btnFloatObject = Floatingactionbuttonparam(\n,
+                  child: ${paramValues.child},\n,
+                  foregroundColor: Colors.${paramValues.foregroundColor},\n,
+                  backgroundColor: Colors.${paramValues.backgroundColor},\n,
+                  mini: ${paramValues.mini},\n,
+                  shape: CircleBorder(),\n,
+                  hoverColor: Colors.${paramValues.hoverColor}\n,
+                  );
+              `,
+        },
+      ];
+      for (i of widgetList){
+          if (i.name == widgetName){
+            resolve({
+                imports: i.imports,
+                call: i.call,
+                definitions: i.definitions
+            });
+          }
+      }
+});
+
+module.exports = getWidgets

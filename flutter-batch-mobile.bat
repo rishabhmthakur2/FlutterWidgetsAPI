105b3f28ca549b05128333e2d958ffbc34021843
rishabhmthakur2 <rishabhmthakur@outlook.com>
2020-08-28 13:49:30 +0530

    Updated batch file config

--- flutter-batch-mobile.bat
+++ flutter-batch-mobile.bat
@@ -1,13 +1,7 @@
 @echo off
 echo  Welcome to Flutter
 
-<<<<<<< HEAD
-cd D:\Cms\NewAPI\FlutterWidgetsAPI\flutter\api_demo
-flutter run
-cd D:\Cms\NewAPI\FlutterWidgetsAPI\flutter\api_demo\android && ./gradlew clean && pause>nul
-=======
 cd ./flutter/api_demo
 flutter run
 cd ./flutter/api_demo/android && ./gradlew clean && pause>nul
->>>>>>> 549219fa866d4d34c57f79532bee05f6460b8b32
 @pause
